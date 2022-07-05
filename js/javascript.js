d7cdbd78d00ef4b92a79ce594243e35f7827be91
fuatio <fuat@fuat.io>
2022-07-05 19:47:48 -0400

    added collapsable sidebar, used watch?v=wEfaoAa99XY, still lots of issues

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+// Open Sidebar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+// Close Sidebar
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
