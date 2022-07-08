8416c86f594191e52f578d1985eae4b7ab5a67a8
fuatio <fuat@fuat.io>
2022-07-07 22:10:44 -0400

    added func to change persistent data js to flask and back

--- app.py
+++ app.py
@@ -25,11 +25,23 @@ tab_content = [
 def home():
     data = list(filter(lambda x: x['city'] == 'London', tab_content))
     if request.method == "POST":
-        post_data = request.form.to_dict()
-        city_name = post_data['city']
-        data = list(filter(lambda x: x['city'] == city_name, tab_content))
-        return jsonify(data)
-    
+        data = request.form.to_dict()
+        if 'city' in data:
+            city_name = data['city']
+            data = list(filter(lambda x: x['city'] == city_name, tab_content))
+            return jsonify(data)
+        elif 'content' in data:
+            print(data)
+            open_city_tab = data['open_city_tab']
+            print(open_city_tab)
+            city_content = data['content']
+            for item in tab_content:
+                if item['city'] == open_city_tab:
+                    item['content'] = city_content
+            data = list(filter(lambda x: x['city'] == open_city_tab, tab_content))
+            return jsonify(data)
+            
+
     return render_template('index.html', tab_names = tab_content)
 
 if __name__ == "__main__":
